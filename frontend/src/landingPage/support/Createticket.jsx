a2bd99458092ad763e497e7860098883d747244e
mangeshwahatule <wahatulemangesh07@gmail.com>
2026-02-25 15:38:35 +0530

    feat: build support page hero section

--- frontend/src/landingPage/support/Createticket.jsx
+++ frontend/src/landingPage/support/Createticket.jsx
@@ -1,7 +1,153 @@
 export default function CreateTicket() {
   return (
-    <div>
-      <h2>CreateTicket</h2>
+    <div className="container">
+      <div className="row p-5 mt-5 mb-5">
+        <h1 className="fs-2">To create a ticket, select a relevant topic</h1>
+
+        {/* Account Opening */}
+        <div className="col-4 p-5 mt-2 mb-2">
+          <h4>
+            <i className="fa fa-plus-circle"></i> Account Opening
+          </h4>
+
+          <a className="text-dark text-decoration-none d-block mb-2" href="">
+            Online Account Opening
+          </a>
+          <a className="text-dark text-decoration-none d-block mb-2" href="">
+            Offline Account Opening
+          </a>
+          <a className="text-dark text-decoration-none d-block mb-2" href="">
+            Company / Partnership / HUF Account
+          </a>
+          <a className="text-dark text-decoration-none d-block mb-2" href="">
+            NRI Account Opening
+          </a>
+          <a className="text-dark text-decoration-none d-block mb-2" href="">
+            Account Opening Charges
+          </a>
+          <a className="text-dark text-decoration-none d-block mb-2" href="">
+            3-in-1 Account Setup
+          </a>
+        </div>
+
+        {/* Your AstraX Account */}
+        <div className="col-4 p-5 mt-2 mb-2">
+          <h4>
+            <i className="fa-solid fa-user"></i> Your AstraX Account
+          </h4>
+
+          <a className="text-dark text-decoration-none d-block mb-2" href="">
+            Login Issues
+          </a>
+          <a className="text-dark text-decoration-none d-block mb-2" href="">
+            Password / PIN Reset
+          </a>
+          <a className="text-dark text-decoration-none d-block mb-2" href="">
+            Update Email / Mobile
+          </a>
+          <a className="text-dark text-decoration-none d-block mb-2" href="">
+            Account Modification
+          </a>
+          <a className="text-dark text-decoration-none d-block mb-2" href="">
+            Deactivate / Reactivate Account
+          </a>
+          <a className="text-dark text-decoration-none d-block mb-2" href="">
+            Profile & Personal Details
+          </a>
+        </div>
+
+        {/* Trading & Platforms */}
+        <div className="col-4 p-5 mt-2 mb-2">
+          <h4>
+            <i className="fa-solid fa-chart-column"></i> Trading & Platforms
+          </h4>
+
+          <a className="text-dark text-decoration-none d-block mb-2" href="">
+            Order Placement Issues
+          </a>
+          <a className="text-dark text-decoration-none d-block mb-2" href="">
+            Charting / Data Issues
+          </a>
+          <a className="text-dark text-decoration-none d-block mb-2" href="">
+            App / Web Platform Issues
+          </a>
+          <a className="text-dark text-decoration-none d-block mb-2" href="">
+            Margin Requirements
+          </a>
+          <a className="text-dark text-decoration-none d-block mb-2" href="">
+            Positions & PnL Queries
+          </a>
+        </div>
+
+        {/* Funds */}
+        <div className="col-4 p-5 mt-2 mb-2">
+          <h4>
+            <i className="fa-solid fa-wallet"></i> Funds
+          </h4>
+
+          <a className="text-dark text-decoration-none d-block mb-2" href="">
+            Add Funds (Deposit Issues)
+          </a>
+          <a className="text-dark text-decoration-none d-block mb-2" href="">
+            Withdraw Funds (Payout Issues)
+          </a>
+          <a className="text-dark text-decoration-none d-block mb-2" href="">
+            Bank Account Linking
+          </a>
+          <a className="text-dark text-decoration-none d-block mb-2" href="">
+            UPI / Netbanking Issues
+          </a>
+          <a className="text-dark text-decoration-none d-block mb-2" href="">
+            Fund Settlement Delay
+          </a>
+        </div>
+
+        {/* Console */}
+        <div className="col-4 p-5 mt-2 mb-2">
+          <h4>
+            <i className="fa-solid fa-circle-notch"></i> Console
+          </h4>
+
+          <a className="text-dark text-decoration-none d-block mb-2" href="">
+            Tax P&L
+          </a>
+          <a className="text-dark text-decoration-none d-block mb-2" href="">
+            Holdings Not Showing
+          </a>
+          <a className="text-dark text-decoration-none d-block mb-2" href="">
+            Corporate Actions
+          </a>
+          <a className="text-dark text-decoration-none d-block mb-2" href="">
+            Ledger & Statements
+          </a>
+          <a className="text-dark text-decoration-none d-block mb-2" href="">
+            Reports & Downloads
+          </a>
+        </div>
+
+        {/* Coin */}
+        <div className="col-4 p-5 mt-2 mb-2">
+          <h4>
+            <i className="fa-regular fa-circle"></i> Coin (Mutual Funds)
+          </h4>
+
+          <a className="text-dark text-decoration-none d-block mb-2" href="">
+            Buy / Sell MF Issues
+          </a>
+          <a className="text-dark text-decoration-none d-block mb-2" href="">
+            SIP Setup Issues
+          </a>
+          <a className="text-dark text-decoration-none d-block mb-2" href="">
+            NAV / Order Delay
+          </a>
+          <a className="text-dark text-decoration-none d-block mb-2" href="">
+            Switch / Redeem Issues
+          </a>
+          <a className="text-dark text-decoration-none d-block mb-2" href="">
+            Coin App / Web Issues
+          </a>
+        </div>
+      </div>
     </div>
   );
 }
--- frontend/src/landingPage/support/Hero.jsx
+++ frontend/src/landingPage/support/Hero.jsx
@@ -1,7 +1,65 @@
 export default function Hero() {
   return (
-    <div>
-      <h2>Hero</h2>
+    <div
+      className="container py-5 mt-5"
+      style={{ background: "#4169e1", borderRadius: "10px" }}
+    >
+      <div className="row pt-5 text-center text-white">
+        <div className="col-6 d-flex flex-column align-items-center">
+          <b className="fs-4 pb-2">Support Portal</b>
+
+          <h5 className="mb-4 w-75">
+            Search for an answer or browse help topics to create a ticket
+          </h5>
+
+          <input
+            type="text"
+            className="form-control w-75 mb-4"
+            placeholder="Eg: how do I activate F&O, why is my order getting rejected..."
+            style={{ borderRadius: "8px" }}
+          />
+
+          <div className="w-75 text-start mt-3">
+            <a
+              href="#"
+              className="d-block text-white mb-2 text-decoration-none"
+            >
+              Track account opening
+            </a>
+            <a
+              href="#"
+              className="d-block text-white mb-2 text-decoration-none"
+            >
+              Track segment activation
+            </a>
+            <a
+              href="#"
+              className="d-block text-white mb-2 text-decoration-none"
+            >
+              Intraday margins
+            </a>
+            <a href="#" className="d-block text-white text-decoration-none">
+              Kite user manual
+            </a>
+          </div>
+        </div>
+
+        {/* RIGHT SIDE */}
+        <div className="col-6 d-flex flex-column align-items-start ps-5">
+          <a href="#" className="text-white text-decoration-none mb-3">
+            <b className="fs-5">Track Tickets</b>
+          </a>
+
+          <h5 className="mb-3">Featured</h5>
+
+          <ul className="text-start">
+            <li className="mb-2">
+              Current Takeovers and Delisting - January 2024
+            </li>
+            <li>Latest Intraday leverages - MIS & CO</li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 }
