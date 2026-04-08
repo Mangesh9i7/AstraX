a4b5ac884df5623212472f9627d7fdef55991751
Wahatule Mangesh <wahatulemangesh07@gmail.com>
2026-04-08 13:09:33 +0530

    Remove import statement for VerticalGraph component

--- dashboard/src/components/Holdings.jsx
+++ dashboard/src/components/Holdings.jsx
@@ -1,6 +1,5 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
-import { VerticalGraph } from "./VerticalGraph";
 
 const Holdings = () => {
   const [holdings, setHoldings] = useState([]);
